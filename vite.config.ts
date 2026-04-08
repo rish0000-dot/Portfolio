df2a980207ea6068fd4cd570570d251c5839fcd4
Rishabh Sharma <rs9076738@gmail.com>
2026-04-08 16:26:20 +0530

    chore: increase Vite chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ export default defineConfig({
     host: true,
     port: 5173,
   },
+  build: {
+    chunkSizeWarningLimit: 2000, // यह 3D libraries (Three.js) की बड़ी फाइल्स के लिए warning को छुपा देगा
+  }
 })
